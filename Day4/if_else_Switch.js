4c5b51445d3d36d282a6a8153493d7a3d98125c2
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-10-07 00:17:49 +0530

    Create if_else_Switch.js

---
+++ Day4/if_else_Switch.js
@@ -0,0 +1,25 @@
+let a = 0
+if (a > 0) {
+  console.log(`${a} is a positive number`)
+} else if (a < 0) {
+  console.log(`${a} is a negative number`)
+} else if (a == 0) {
+  console.log(`${a} is zero`)
+} else {
+  console.log(`${a} is not a number`)
+}
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+let weather = 'cloudy'
+switch (weather) {
+  case 'rainy':
+    console.log('You need a rain coat.')
+    break
+  case 'cloudy':
+    console.log('It might be cold, you need a jacket.')
+    break
+  case 'sunny':
+    console.log('Go out freely.')
+    break
+  default:
+    console.log(' No need for rain coat.')
+}
