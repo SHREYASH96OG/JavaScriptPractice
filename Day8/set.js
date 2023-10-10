4897665f4b6b8aba3e8d57693267076a7076f6b0
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-10-10 23:18:49 +0530

    Create set.js

---
+++ Day8/set.js
@@ -0,0 +1,12 @@
+const languages = [
+  'English',
+  'Finnish',
+  'English',
+  'French',
+  'Spanish',
+  'English',
+  'French',
+]
+
+const setOfLanguages = new Set(languages)
+console.log(setOfLanguages)
