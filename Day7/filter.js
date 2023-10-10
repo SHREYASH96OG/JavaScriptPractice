f19f25279e4f69b25d1504fcea4f87d1d11b1832
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-10-10 23:10:35 +0530

    Create filter.js

---
+++ Day7/filter.js
@@ -0,0 +1,11 @@
+const scores = [
+  { name: 'Asabeneh', score: 95 },
+   { name: 'Lidiya', score: 98 },
+  { name: 'Mathias', score: 80 },
+  { name: 'Elias', score: 50 },
+  { name: 'Martha', score: 85 },
+  { name: 'John', score: 100 },
+]
+
+const scoresGreaterEighty = scores.filter((score) => score.score > 80)
+console.log(scoresGreaterEighty)
