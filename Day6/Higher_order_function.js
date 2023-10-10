cf56a49e4d7e5d0ba0fbf3c63b8e10afa82b4817
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-10-10 22:44:22 +0530

    Create Higher_order_function.js

---
+++ Day6/Higher_order_function.js
@@ -0,0 +1,11 @@
+// a callback function, the name of the function could be any name
+const callback = (n) => {
+  return n ** 2
+}
+​
+// function that takes other function as a callback
+function cube(callback, n) {
+  return callback(n) * n
+}
+​
+console.log(cube(callback, 3))
