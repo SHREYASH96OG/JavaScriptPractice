3a102dfdbfe05c0c8587b0ace849db93ce448989
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-10-10 23:24:08 +0530

    Update set.js

--- Day8/set.js
+++ Day8/set.js
@@ -10,3 +10,25 @@ const languages = [
 
 const setOfLanguages = new Set(languages)
 console.log(setOfLanguages)
+
+// ----------------------------------------------
+const companies = new Set() // creating an empty set
+console.log(companies.size) // 0
+
+companies.add('Google') // add element to the set
+companies.add('Facebook')
+companies.add('Amazon')
+companies.add('Oracle')
+companies.add('Microsoft')
+console.log(companies.size) // 5 elements in the set
+console.log(companies)
+
+console.log(companies.delete('Google'))
+console.log(companies.size) // 4 elements left in the set
+
+// ----------------------------------------------
+console.log(companies.has('Apple')) // false
+console.log(companies.has('Facebook')) // true
+
+companies.clear()
+console.log(companies)
