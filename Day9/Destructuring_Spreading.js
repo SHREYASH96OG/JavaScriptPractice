7345f0223a0827513ebfb72e64e8ae000ebfc833
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-10-12 16:16:57 +0530

    Update and rename Destructuring.js to Destructuring_Spreading.js

--- Day9/Destructuring_Spreading.js
+++ Day9/Destructuring_Spreading.js
@@ -12,3 +12,10 @@ const rectangle = {
 let { width, height, area, perimeter } = rectangle
 
 console.log(width, height, area, perimeter) //20 10 200 undefined
+
+// ----------------------------------------------------------------
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let [num1, num2, num3, ...rest] = nums
+​
+console.log(num1, num2, num3) //1 2 3
+console.log(rest)//[4, 5, 6, 7, 8, 9, 10]
