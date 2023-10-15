1a88c718820cd26f6f980abb49d9f4be5ff9429e
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-10-15 23:26:19 +0530

    Create Inheritance.js

---
+++ Day12/Inheritance.js
@@ -0,0 +1,11 @@
+class Student extends Person {
+  saySomething() {
+    console.log('I am a child of the person class')
+  }
+}
+
+const s1 = new Student('Asabeneh', 'Yetayeh', 'Finland', 250, 'Helsinki')
+console.log(s1)
+console.log(s1.saySomething())
+console.log(s1.getFullName())
+console.log(s1.getPersonInfo())
